0e99461e80d608bf7be43d1482c7a83943710ba2
Santox <santox@debian-BULLSEYE-live-builder-AMD64>
2022-10-03 18:39:40 -0300

    changes on tse

--- app/backend/src/controllers/robots/tse.js
+++ app/backend/src/controllers/robots/tse.js
@@ -1,10 +1,11 @@
-const Axios = require('Axios')
-, config = {
+const
+START       = true
+, Axios     = require('Axios')
+, config    = {
     host_ : "https://resultados.tse.jus.br"
     , uri_: "/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json"
 }
 ;;
-
 
 module.exports = class Tse {
 
